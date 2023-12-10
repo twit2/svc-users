6101be2d9c8b24c715fd315adc793a6564133020
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-12-10 19:40:52 +0100

    Configure code coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/src/models",
+        "/src/op",
+        "/src/routes",
+        "/src/rpc"
+    ]
 };
